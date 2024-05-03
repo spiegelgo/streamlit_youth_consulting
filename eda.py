258a86c1dae059d8ba83dbc8ef33f77f08dca8e5
spiegelgo <167832064+spiegelgo@users.noreply.github.com>
2024-05-03 16:01:37 +0900

    문구 수정

--- eda.py
+++ eda.py
@@ -17,21 +17,27 @@ def run_eda():
     if platform.system() == 'Linux':
         rc('font', family='NanumGothic')
             
-    st.subheader('데이터프레임 확인하기')
+    st.subheader('전체 데이터프레임 확인')
     df = pd.read_csv('./data/youth_consulting.csv', index_col=0)
     #print(df)
     
     st.dataframe(df)
 
-    st.subheader('상담유형별 지역데이터의 시각화')
-    st.text('상담유형을 선택하면 지역별 데이터를 보여드립니다.')
+    st.subheader('상담유형별 지역순 시각화')
+    st.text('상담유형을 선택하면 데이터프레임과 함께, 해당 유형이 가장 많은 지역순으로 보여드립니다.')
+    st.text('줌인 줌아웃이 가능하며 원형 그래프의 경우 우측의 원하는 항목을 제거하거하거나 되돌릴 수 있습니다')
     
+    
+
     available_columns = [col for col in df.columns if col not in ['지역', '합계']] # 컬럼 선택시 지역과 합계는 제외
     selected_columns = st.multiselect('상담 유형을 선택하세요', available_columns)
 
     df_filtered = df[df['지역'] != '전국'] # 지역 컬럼에서의 전국 데이터는 제외한 지역리스트
-
+    # 선택한 '상담유형'의 데이터프레임 출력
     if selected_columns:
+        selected_df = df_filtered[selected_columns + ['지역']]  # '지역' 열을 포함하여 선택된 열만 필터링
+        selected_df.set_index('지역', inplace=True)  # '지역'을 인덱스로 설정
+        st.write(selected_df)
         # 선택한 컬럼과 '지역' 컬럼의 데이터 시각화
         for column in selected_columns:
             # 바 차트 그리기
@@ -40,17 +46,19 @@ def run_eda():
             st.plotly_chart(fig_bar)
 
             # 파이 차트 그리기
-            fig_pie = px.pie(df_filtered, names='지역', values=column, title=f'{column} 관련 유형 의 지역별 Pie 차트', hole=0.2,)
+            fig_pie = px.pie(df_filtered, names='지역', values=column, title=f'{column} 관련 유형 상담의 지역별 퍼센테이지', hole=0.2,)
             st.plotly_chart(fig_pie)
     else:
         st.error('상담유형을 선택해주세요.')
 
     
-
     #-----------------------------------------------------------
-    st.subheader('지역별 상담유형 데이터와 시각화')
-    st.text('지역을 선택하면 상담유형별 데이터를 보여드립니다.')
+    
+    st.subheader('지역별 상담유형순 시각화')
+    st.text('지역을 선택하면 선택한 데이터프레임과 함께 가장 많은 상담유형순서로 보여드립니다.')
     st.text('2개 이상 선택시 결과는 합산해서 보여집니다')
+    st.text('줌인 줌아웃이 가능하며 원형 그래프의 경우 우측의 원하는 항목을 제거하거하거나 되돌릴 수 있습니다')
+    
     choice_list = st.multiselect('지역을 선택하세요', df['지역'])
     if choice_list :
         selected_rows = df[df['지역'].isin(choice_list)]
@@ -58,13 +66,6 @@ def run_eda():
 
         combined_data = selected_rows.drop(columns=['지역', '합계']).sum(axis=0)
 
-        max_value = combined_data.max()
-        min_value = combined_data.min()
-        max_column = combined_data.idxmax()
-        min_column = combined_data.idxmin()
-
-        st.success(f"선택한 지역의 가장 많은 유형은 : {max_column} 관련의 상담으로 {format(max_value, ',')} 건 입니다.")
-        st.warning(f"선택한 지역의 가장 적은 유형은 : {min_column} 관련의 상담으로 {format(min_value, ',')} 건 입니다.")
 
         st.subheader('시각화')
         st.text('위에서 선택한 지역을 차트로 확인하자')
