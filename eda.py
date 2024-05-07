91e7a4ed50ef8011c2c4465760c886ddb99c3ee4
spiegelgo <167832064+spiegelgo@users.noreply.github.com>
2024-05-07 11:41:38 +0900

    텍스트 편집

--- eda.py
+++ eda.py
@@ -22,11 +22,11 @@ def run_eda():
     st.dataframe(df)
 
     st.subheader('청소년 고민상담유형 차트 1')
-    st.text('상담유형을 선택하면 데이터프레임과 함께')
-    st.text('해당 유형이 가장 많은 지역순과 백분율로 보여드립니다.')
-    st.text('2개 이상 선택시 선택한 상담수를 합산하여 지역별 그래프를 추가로 보여드립니다')
-    st.text('줌인 줌아웃이 가능하며 원형 그래프의 경우')
-    st.text('우측의 원하는 항목을 제거하거하거나 되돌릴 수 있습니다.')
+    st.markdown('상담유형을 선택하면 선택한 상담유형만의 데이터프레임과 함께')
+    st.markdown('해당 유형이 가장 많은 지역순과 백분율로 보여드립니다.')
+    st.markdown('2개 이상 선택시 선택한 상담수를 합산하여 지역별 그래프를 추가로 보여드립니다')
+    st.markdown('줌인 줌아웃이 가능하며 원형 그래프의 경우')
+    st.markdown('우측의 원하는 항목을 제거하거하거나 되돌릴 수 있습니다.')
     
     
     available_columns = [col for col in df.columns if col not in ['지역', '합계']] # 컬럼 선택시 지역과 합계는 제외
@@ -48,22 +48,22 @@ def run_eda():
 
             # 바 차트 그리기 ( 합산)
             selected_df = selected_df.sort_values(by='합산', ascending=False)
-            fig_bar1 = px.bar(selected_df, x=selected_df.index, y='합산', title=f'{", ".join(selected_columns)} 상담수 총합')
+            fig_bar1 = px.bar(selected_df, x=selected_df.index, y='합산', title=f'{", ".join(selected_columns)} 고민상담수 총합')
             st.plotly_chart(fig_bar1)
 
             # 파이 차트 그리기 ( 합산 비율)
-            fig_pie1 = px.pie(selected_df, names=selected_df.index, values='합산', title=f'{", ".join(selected_columns)} 상담의 지역 비율', hole=0.2)
+            fig_pie1 = px.pie(selected_df, names=selected_df.index, values='합산', title=f'{", ".join(selected_columns)} 고민상담의 지역 비율', hole=0.2)
             st.plotly_chart(fig_pie1)
 
         # 선택된 컬럼들의 각각의 차트 생성
         for column in selected_columns:
             # 바 차트 그리기
             sorted_df = df_filtered.sort_values(by=column, ascending=False)
-            fig_bar = px.bar(sorted_df, x='지역', y=column, title=f'{column} 상담 수')
+            fig_bar = px.bar(sorted_df, x='지역', y=column, title=f'지역별 {column} 고민 상담 수')
             st.plotly_chart(fig_bar)
 
             # 파이 차트 그리기
-            fig_pie = px.pie(df_filtered, names='지역', values=column, title=f'{column} 상담의 지역 비율', hole=0.2)
+            fig_pie = px.pie(df_filtered, names='지역', values=column, title=f'{column} 고민상담의 지역 비율', hole=0.2)
             st.plotly_chart(fig_pie)
 
     # 선택된 컬럼이 없는 경우 에러 메시지 출력
@@ -75,11 +75,11 @@ def run_eda():
     #-----------------------------------------------------------
     
     st.subheader('청소년 고민상담유형 차트 2')
-    st.text('지역을 선택하면 선택한 데이터프레임과 함께')
-    st.text('가장 많은 고민상담유형순서와 백분율로 보여드립니다.')
-    st.text('2개 이상 선택시 결과는 합산해서 보여집니다.')
-    st.text('줌인 줌아웃이 가능하며 원형 그래프의 경우')
-    st.text('원하는 항목을 제거하거하거나 되돌릴 수 있습니다.')
+    st.markdown('지역을 선택하면 선택한 데이터프레임과 함께')
+    st.markdown('가장 많은 고민상담유형순서와 고민상담유형의 비율을 보여드립니다.')
+    st.markdown('2개 이상 선택시 결과는 합산해서 보여집니다.')
+    st.markdown('줌인 줌아웃이 가능하며 원형 그래프의 경우')
+    st.markdown('원하는 항목을 제거하거하거나 되돌릴 수 있습니다.')
     
     choice_list = st.multiselect('지역을 선택하세요', df['지역'])
     if choice_list :
@@ -97,11 +97,11 @@ def run_eda():
         # 바 차트 그리기
         sorted_values = sorted(values, reverse=True)
         sorted_labels = [x for _, x in sorted(zip(values, labels), reverse=True)]
-        fig1 = px.bar(x=sorted_labels, y=sorted_values, title=f'{", ".join(choice_list)} 의 상담수')
+        fig1 = px.bar(x=sorted_labels, y=sorted_values, title=f'{", ".join(choice_list)} 의 고민상담 수')
         st.plotly_chart(fig1)
 
         # 파이차트로 그려보자
-        fig2 = px.pie(names=labels, values=values, title=f'{", ".join(choice_list)} 의 상담 백분율', hole=0.2)
+        fig2 = px.pie(names=labels, values=values, title=f'{", ".join(choice_list)} 의 고민상담 비율', hole=0.2)
         st.plotly_chart(fig2)    
 
 
--- home.py
+++ home.py
@@ -3,9 +3,9 @@ import streamlit as st
 def run_home():
     st.subheader('지역별, 상담유형별로 청소년 고민상담 현황을 알아보자')
     
-    st.text("'한국청소년활동진흥원'에서 제공한")
-    st.text('지역별 상담 유형 통계 정보(2019).csv 파일을 통해 만들어졌습니다')
+    st.markdown("'한국청소년활동진흥원'에서 제공한")
+    st.markdown('지역별 상담 유형 통계 정보(2019).csv 파일을 통해 만들어졌습니다')
     st.link_button('해당 데이터셋은 이 곳에서 받았습니다','https://www.bigdata-culture.kr/bigdata/user/data_market/detail.do?id=6aa14c34-4866-4a3f-9223-09c12d58ad4b')
-    st.text('데이터 설명 : 청소년 상담시 청소년들이 주로 물어보는 카테고리의 지역별 건수')
-    st.text('활용분야 : 특정지역의 청소년들이 고민하는 유형을 분석하여 정책 방향 및 전문 상담사 배정시 활용')
+    st.markdown('데이터 설명 : 청소년 상담시 청소년들이 주로 물어보는 카테고리의 지역별 건수')
+    st.markdown('활용분야 : 특정지역의 청소년들이 고민하는 유형을 분석하여 정책 방향 및 전문 상담사 배정시 활용')
     st.image('./image/consulting.png',use_column_width = True)
